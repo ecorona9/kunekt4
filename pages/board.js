87969263715637c67fc1101eb97e47ebeaa2e248
ecorona9 <79238407+ecorona9@users.noreply.github.com>
2022-05-20 15:17:27 -0700

    last commit

--- pages/board.js
+++ pages/board.js
@@ -15,8 +15,15 @@ export default function Board() {
     const [turn, setTurn] = useState(1);
     const [gameOver, setGameOver] = useState(false);
     const [mode, setMode] = useState(0);
+    const [count, setCount] = useState(false);
+    const [redTurn, setRedTurn] = useState(false);
 
     var handleClick = (lane, i) => {
+        setCount(count => count + 1);
+        // var newTurn = turn * -1;
+        setTurn(turn => -turn);
+        setRedTurn(redTurn => !redTurn);
+        
         if(gameOver)
             return;
         var copy = [...board];
@@ -32,20 +39,13 @@ export default function Board() {
             return;
 
         console.log('current mode: ', mode);
-        if(turn == 1)
-            setTurn(-1);
-        else
-            setTurn(1);
+        
         if(mode == 1)
         {
             console.log('random clicking');
-            setTimeout(() => { randomClick(); }, 5000);
-            
-            if(turn == 1)
-                setTurn(-1);
-            else
-                setTurn(1);
-
+            setTurn(turn => -1);
+            setTimeout(() => { randomClick(); 
+            }, 1000);
         }
             
 
@@ -54,6 +54,10 @@ export default function Board() {
     }
 
     var randomClick = () => {
+        setCount(count => count + 1);
+        setRedTurn(redTurn => !redTurn)
+        setTurn(turn => -turn);
+        // setTurn(turn => -1);
         var copy = [...board];
         var i = Math.floor(Math.random() * 7);
         while(copy[i].lastIndexOf(0) < 0) {
@@ -74,6 +78,7 @@ export default function Board() {
             return;
         // console.log(board);
         // console.log('turn:', turn)
+
     }
 
     
@@ -86,7 +91,8 @@ export default function Board() {
             [0,0,0,0,0,0],
             [0,0,0,0,0,0],
             [0,0,0,0,0,0]]);
-        turn = 1;
+        setTurn(1);
+        setCount(0);
         setGameOver(false);
     }
 
@@ -152,11 +158,150 @@ export default function Board() {
             }
         }
         return false;
-    }    
+    } 
+
+    var checkGameState = (state, tempBoard) => {
+        // Check if board has been filled
+        var player = 0;
+        if(state === "max")
+            player = -1;
+        if(state === "min")
+            player = 1;
+
+        var foundZero = false;
+        for(var i = 0; i < tempBoard.length; i++) {
+            var lane = tempBoard[i];
+            for(var j = 0; j < lane.length; j++) {
+                if(lane[j] == 0)
+                    foundZero = true;
+            }
+        }
+        if(!foundZero)
+        {
+            return 0;
+        }
+            
+            
+
+        // Check if anyone has won
+        // vertical 
+        for (var j = 0; j<tempBoard.length-3 ; j++ ){
+            for (var i = 0; i< tempBoard[j].length; i++){
+                if (tempBoard[i][j] == player && tempBoard[i][j+1] == player && tempBoard[i][j+2] == player && tempBoard[i][j+3] == player){
+                    return player;
+                }           
+            }
+        }
+        // horizontal
+        for (var i = 0; i<tempBoard.length-3 ; i++ ){
+            for (var j = 0; j<tempBoard[i].length; j++){
+                if (tempBoard[i][j] == player && tempBoard[i+1][j] == player && tempBoard[i+2][j] == player && tempBoard[i+3][j] == player){
+                    return player;
+                }           
+            }
+        }
+        // ascending 
+        for (var i=3; i<tempBoard.length; i++){
+            var lane = tempBoard[i];
+            for (var j=0; j<lane.length-3; j++){
+                if (tempBoard[i][j] == player && tempBoard[i-1][j+1] == player && tempBoard[i-2][j+2] == player && tempBoard[i-3][j+3] == player) {
+                    return player;
+                }
+            }
+        }
+        // descending
+        for (var i=3; i<tempBoard.length; i++){
+            var lane = tempBoard[i];
+            for (var j=3; j<lane.length; j++){
+                if (tempBoard[i][j] == player && tempBoard[i-1][j-1] == player && tempBoard[i-2][j-2] == player && tempBoard[i-3][j-3] == player) {
+                    return player;
+                }
+            }
+        }
+        return 0;
+    } 
+
+
+    var getMoves = (copy) => {
+        var result = [];
+        for(var i = 0 ; i < 8 ; i++)
+        {
+            if(copy[i].lastIndexOf(0) >= 0)
+            {
+                result.concat(i);
+            }
+        }
+        return result;
+    }
+
+    var minimax = (currBoard, depth, a, b, maxPlayer) => {
+        var moves = getMoves(currBoard);
+        var terminal = isTerminal(currBoard);
+
+        if(depth === 0 || terminal)
+        {
+            if(terminal)
+            {
+                if(wins(currBoard, -1))
+                    return [-1, 1];
+                else if(wins(currBoard, 1))
+                    return [-1, -1];
+                else  
+                    return [-1, 0];
+            }
+            else
+                return [-1, 0];
+        }
+
+        if(maxPlayer)
+        {
+            var v = -100000;
+            var selectedCol = 0;
+            for(var i = 0 ; i < moves.length ; i++)
+            {
+                var copy = [...currBoard];
+                copy[moves[i]][copy[moves[i]].lastIndexOf(0)] = -1;
+                var newValue = minimax(copy, d-1, a, b, false)[1];
+                if(newValue > v)
+                {
+                    v = newValue;
+                    selectedCol = moves[i];
+                }
+                a = max(a, value);
+                if(a >= b)
+                    break;
+            }
+            return [selectedCol, v];
+        }
+        else
+        {
+            var v = 100000;
+            var selectedCol = 0;
+            for(var i = 0 ; i < moves.length ; i++)
+            {
+                var copy = [...currBoard];
+                copy[moves[i]][copy[moves[i]].lastIndexOf(0)] = 1;
+                var newValue = minimax(copy, d-1, a, b, true)[1];
+                if(newValue > v)
+                {
+                    v = newValue;
+                    selectedCol = moves[i];
+                }
+                b = max(b, value);
+                if(a >= b)
+                    break;
+            }
+            return [selectedCol, v];
+        }
+
+    }
 
   return (
       <>
         <div className="gameState">
+            redTurn: {redTurn ? "true" : "false"}
+            count: {count}
+            current turn: {turn}
             <button onClick={() => resetBoard()}> RESET </button>
             <div onChange={(e) => setMode(e.target.value)}>
                 <input type="radio" value={0} name="mode" /> Player
